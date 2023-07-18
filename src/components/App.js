308a9dc41943f858a85b554c7a553b9bbfdfe23c
Fayeemahmad398 <fayeemahmad0@gmail.com>
2023-07-18 22:14:00 +0530

    first

--- src/components/App.js
+++ src/components/App.js
@@ -1,13 +1,63 @@
-
-import React from "react";
-import './../styles/App.css';
-
+import React, { useState } from "react";
+import "./../styles/App.css";
+import axios from "axios";
+const apikey = `1615fe9d`;
 const App = () => {
+  const [data, setData] = useState([]);
+  const [search, setSearch] = useState("");
+  const [isFound, setFound] = useState("");
+  const url = `https://www.omdbapi.com/?&apikey=${apikey}&s=${search}`;
+  function fetchData() {
+    axios
+      .get(url)
+      .then((response) => {
+        console.log(response.data);
+        if (response.data.Response === "True") {
+          setData(response.data.Search);
+          setFound("");
+        } else if (response.data.Response === "False") {
+          setFound("Invalid movie name. Please try again.");
+          setData({});
+        }
+      })
+      .catch((error) => {
+        console.log(error);
+      });
+  }
   return (
     <div>
-        {/* Do not remove the main div */}
+      <h3>Search movie</h3>
+      <div>
+        <input
+          type="text"
+          onChange={(event) => {
+            setSearch(event.target.value);
+          }}
+        />
+        <button onClick={fetchData}>Search</button>
+      </div>
+      {data.length > 0 ? (
+        <div>
+          {data.map((obj) => {
+            return (
+              <div>
+                <ul>
+                  <li>
+                    <h2>
+                      {obj.Title} {`(${obj.Year})`}
+                    </h2>
+                  </li>
+                </ul>
+                <img src={obj.Poster} alt="" />
+              </div>
+            );
+          })}
+        </div>
+      ) : (
+        <div className="error">{isFound}</div>
+      )}
     </div>
-  )
-}
+  );
+};
 
-export default App
+export default App;
